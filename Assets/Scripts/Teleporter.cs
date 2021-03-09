f0330e26ea90728903f60726cfa9d7f0710837fb
Ivan Santiago Gutierrez Estrada <55468122+TheTerribleS@users.noreply.github.com>
2021-03-08 21:59:08 -0600

    teleport fix

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public Vector3 targetPosition;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Move>() != null)
+        {
+            other.transform.position = targetPosition;
+            Camera.main.transform.position = targetPosition;
+        }
+    }
+}
